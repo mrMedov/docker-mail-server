b30c24b60e230c685ebd4b1b6dab2095e8d1b896
Viktor Medov <rendjey@ya.ru>
2024-06-25 13:32:44 +0300

    .v.0.9.4.Minor volumes/etc/ Commit.mailserver

---
+++ volumes/etc/postfixadmin/dbconfig.inc.php
@@ -0,0 +1,12 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/postfixadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+
